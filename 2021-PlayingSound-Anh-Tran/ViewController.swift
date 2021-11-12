f62f36db8f27d14dbf5d6267c2f55692b7a2987e
Anh Tran <lamanh309@gmail.com>
2021-11-11 22:17:41 -0800

    FINAL

--- 2021-PlayingSound-Anh-Tran/ViewController.swift
+++ 2021-PlayingSound-Anh-Tran/ViewController.swift
@@ -12,57 +12,80 @@ class ViewController: UIViewController {
     
     var audioPlayer: AVAudioPlayer!
     
-    @IBOutlet weak var foodImageView: UIImageView!
+    
+    @IBOutlet weak var houseImageView: UIImageView!
     
     
-    struct Dish {
-        var Index: Int
-        var name: String
+    struct HouseModel {
+        var index: Int
+        var address: String
         var price: Double
         var imageName: String
         var soundName: String
     }
     
-    let foodMenu = [
-        Dish(Index: 0, name: "Pizza", price: 19.99, imageName: "pizza", soundName: "movie"),
-        Dish(Index: 1, name: "Hamburger", price: 15.99, imageName: "hamburger", soundName: "magic"),
-        Dish(Index: 2, name: "pasta", price: 18.99, imageName: "pasta", soundName: "cheering01")]
+    let houseListing = [
+           HouseModel(index: 0, address: "1600 Holloway Ave, San Francisco", price: 999999.99, imageName: "house01", soundName: "cheering01"),
+           HouseModel(index: 1, address: "835 Market Street, San Francisco", price: 1599989.99, imageName: "house02", soundName: "magic"),
+           HouseModel(index: 2, address: "1000 Palm Street, San Francisco", price: 2100000.00, imageName: "house03", soundName: "movie")]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        foodImageView.image = UIImage(named: foodMenu[0].imageName)
+        houseImageView.image = UIImage(named: houseListing[0].imageName)
         // Do any additional setup after loading the view.
         //tag the first image and set the atf to be 0
-        foodImageView.tag = 0
+        houseImageView.tag = 0
     }
-    
-    @IBAction func nextDishButtonPressed(_ sender: UIButton) {
-        
-        var foodIndex = foodImageView.tag + 1
-        if foodIndex == foodMenu.count {
-            foodIndex = 0
-        }
-        
-        let foodImageName = foodMenu[foodIndex].imageName
-        
-        foodImageView.image = UIImage(named: foodImageName)
-        foodImageView.tag = foodIndex
-        
-        let foodSoundName = foodMenu[foodIndex].soundName
-        
-        if let sound = NSDataAsset(name: foodSoundName) {
-            do {// this code wiil stop your app from creashing
+    //create a function to play sound
+        // the function takes one parameter which is the name of the sound file
+    func playSound(soundName: String) {
+            //soundName is actually passed in through the parameter of the function
+        if let sound = NSDataAsset(name: soundName) {
+                //if I can find this sound clip
+                //the best thing i can do is try to play it, if not, I will catch the erro
+            do { //this code will stop your app from crashing
                 try audioPlayer = AVAudioPlayer(data: sound.data)
-                //if we successfully access the sound  data we are going to play it
-                audioPlayer.play()
-            } catch {
-                print("Error: Cannot play the sound")
-            }
-        } else {
-            print("Error: cannot access sound clip called movie")
-        }
-        // if I can find this is sound clip
-    }
-}
+                    //if we successfully access the sound data we are going to play it
+                    audioPlayer.play()
+                } catch {
+                print("😡 Error: cannot play the sound clip")
+                }//end of catch
+                
+        } //end of if let
+        else {
+                //if I cannot find this sound clip
+            print("😡 Error: cannot access sound clip called movie")
+        } //end of else
+    } //end of function
+    
+   
+    
+    
+    
+    
+    @IBAction func nextHouseButtomPressed(_ sender: UIButton) {
+           
+        var houseIndex = houseImageView.tag + 1
+               
+               //figure out which is the next dish to display
+               if houseIndex == houseListing.count {
+                   houseIndex = 0
+               }
+               
+               //figure out which image to display
+               let houseImageName = houseListing[houseIndex].imageName
+               
+               houseImageView.image = UIImage(named: houseImageName)
+               //update the tag for your imageview
+               houseImageView.tag = houseIndex
+               
+               //figure out which sound to play
+               let houseSoundName = houseListing[houseIndex].soundName
+               
+               //call playSound() to play sound named foodSoundName
+               playSound(soundName: houseSoundName)
+               
+           }
+       }
 
